3fc2552c197017d27856640e9a88d54ba78155ff
jackunion <verteletskydmitry@gmail.com>
2023-03-03 23:04:28 +0100

    priorityqueue

--- astar.ts
+++ astar.ts
@@ -39,5 +39,54 @@ export const astar = ({
   getNeighbors = defaultGetNeighbors,
   walkFunction,
 }: AStar) => {
-  return []
+  interface ILocation extends Point {
+    priority: number
+  }
+
+  class Location implements ILocation {
+    x: number
+    y: number
+    key: number
+    priority: number
+
+    constructor(point: Point) {
+      this.x = point.x
+      this.y = point.y
+      this.key = (this.y << 16) | this.x
+      this.priority = 0
+    }
+  }
+
+  const startLocation = new Location(start)
+  const endLocation = new Location(end)
+
+  const frontier = new PriorityQueue<Location>(
+    (a, b) => a.priority - b.priority,
+    [new Location(start)],
+  )
+
+  const visited: { [n: number]: Location } = {}
+  const score: { [n: number]: number } = { [startLocation.key]: 0 }
+
+  while (!frontier.isEmpty()) {
+    const current: Location = frontier.pop()!
+
+    if (current.key === endLocation.key) {
+      break
+    }
+
+    getNeighbors(current, 10, 10).forEach((neighbor: Point) => {
+      const next = new Location(neighbor)
+      const nextScore = score[current.key]! + 1
+
+      if (score[next.key] === undefined || nextScore < score[next.key]!) {
+        visited[next.key] = current
+        score[next.key] = nextScore
+        next.priority = nextScore + manhattan(current, next)
+        frontier.push(next)
+      }
+    })
+  }
+
+  return visited
 }
